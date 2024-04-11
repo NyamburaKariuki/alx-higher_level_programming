f782cb39ab6ac5413d6494725b00f9eda5807e2c
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-11 13:10:37 +0300

    loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < lines.length; i++) {
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for ( let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
 }
