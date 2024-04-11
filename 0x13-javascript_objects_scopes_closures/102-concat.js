6c6df3fdf0e4400c3c874d997ca54a1c9790ed5d
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-12 00:26:00 +0300

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file1 = process.argv[2];
-const file2 = process.argv[3];
-const file3 = process.argv[4];
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 
-const data1 = fs.readFileSync(file1, { encoding: 'utf8' });
-const data2 = fs.readFileSync(file2, { encoding: 'utf8' });
-fs.writeFileSync(file3, data1 + data2, { encoding: 'utf8' });
+const dataA = fs.readFileSync(fileA, { encoding: 'utf8' });
+const dataB = fs.readFileSync(fileB, { encoding: 'utf8' });
+fs.writeFileSync(fileC, dataA + dataB, { encoding: 'utf8' });
