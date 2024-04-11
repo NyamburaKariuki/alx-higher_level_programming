40e4a96c7c5141646381c6f8ab1758f73a1e55b1
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-11 14:09:37 +0300

    fixed style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i;
-for ( let i = 0; i < lines.length; i++) {
+for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
 }
