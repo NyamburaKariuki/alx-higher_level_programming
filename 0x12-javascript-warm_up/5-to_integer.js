730da5a3086851b329788c10cf0d2c1ec57270d9
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-11 12:44:43 +0300

    an Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let arg = process.argv[2];
+const process = require('process');
 let num;
+let msg = 'Not a number';
 if (process.argv.length > 2) {
-  num = parseInt(arg);
+  num = parseInt(process.argv[2]);
   if (!isNaN(num)) {
     num = String(num);
-    console.log('my number:', `${num}`);
-} else { 
-  console.log('Not a number');
+    msg = `My number: ${num}`;
+  }
 }
-}
+console.log(msg);
