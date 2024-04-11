c5ed45503e243ba019626da823d7239ae89b8333
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-12 00:17:49 +0300

    Concat files

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,12 +5,12 @@ const occurrencesDict = {};
 
 // Iterate through the data and populate the occurrencesDict
 for (const userId in data.dict) {
-    const occurrences = data.dict[userId];
-    if (!occurrencesDict[occurrences]) {
-        occurrencesDict[occurrences] = [userId];
-    } else {
-        occurrencesDict[occurrences].push(userId);
-    }
+  const occurrences = data.dict[userId];
+  if (!occurrencesDict[occurrences]) {
+    occurrencesDict[occurrences] = [userId];
+  } else {
+    occurrencesDict[occurrences].push(userId);
+  }
 }
 
 // Print the new dictionary
