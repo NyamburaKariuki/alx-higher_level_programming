f6a116f8f7d313d73a678d7ce7c485723b606391
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-15 20:43:19 +0300

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Prints the first State object from the database hbtn_0e_6_us
-         
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
