82077bc371619835f876f54e5af30606e3888bb0
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-18 10:16:47 +0300

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     session = Session()
 
-    states = session.query(State).filter(State.name=argv[4]).first()
+    states = session.query(State).filter_by(State.name=argv[4]).first()
     if states is not None:
         print('{}'.format(state.id))
     else:
