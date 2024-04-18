095d8e93997b611c4fb53b669fed0766392ecd69
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-18 11:40:56 +0300

    modifying

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     s = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-    for s in states:
+    for state in s:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
