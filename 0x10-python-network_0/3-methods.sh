506faaecc1ede2df1c3203fcbe18f90e540fef01
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-25 20:46:11 +0300

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Fetch allowed HTTP methods using curl
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | tr -d '\r' | awk '{print "Allowed HTTP methods:", $2}'
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" " 
