b1aa17779813c1cb5cea12344568f2a720a22ee1
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-27 21:39:16 +0300

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,8 +6,15 @@ found in the header of the response.
 
 
 if __name__ == "__main__":
+   import urllib.request
+    import urllib.parse
     import sys
-    import urllib.request
 
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.headers["X-Request-Id"])
+    argv = sys.argv
+    url = argv[1]
+    email = argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode('ascii')
+
+    with urllib.request.urlopen(url, DATA) as response:
+        print(response.read().decode('utf-8')) 
