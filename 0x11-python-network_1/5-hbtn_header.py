4eaacb0b471e0632fe856787d30a23791c4485fd
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-27 21:00:06 +0300

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,5 +10,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    value = response.headers.get("X-Request-Id")
-    print(value)
+    result = response.headers.get("X-Request-Id")
+    print(result)
