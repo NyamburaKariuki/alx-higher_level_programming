b1837f69e8521a63418af450f239cda32ff8606d
NyamburaKariuki <prettimercii@gmail.cm>
2024-05-24 16:16:02 +0300

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-// Number of films with the given character ID
 const request = require('request');
-let num = 0;
 
-request.get(process.argv[2], (error, response, body) => {
+//  first argument is the API URL
+const URL = process.argv[2];
+
+request(URL, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    content.results.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character.includes(18)) {
-          num += 1;
-        }
-      });
-    });
-    console.log(num);
+  } else if (body) {
+    // Wedge Antilles is character ID 18 - use this ID for filtering the
+    // result of the API
+    const json = JSON.parse(body);
+    const charFilms = json.results.filter(
+      x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+    );
+    console.log(charFilms.length);
   }
 });
